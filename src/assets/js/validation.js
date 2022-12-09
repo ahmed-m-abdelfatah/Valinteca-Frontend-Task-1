f85753d7f1b50833920bc13b3ae9858c8bc7fb67
ahmed-m-abdelfatah <ahmedmo.engineer@gmail.com>
2022-12-09 16:49:05 +0200

    checkpoint

--- src/assets/js/validation.js
+++ src/assets/js/validation.js
@@ -1,61 +1,96 @@
-const validation = () => {
-  // get all Fields
-  const form = document.querySelector('.form');
-  const inputs = {
-    username: document.getElementById('username'),
-    email: document.getElementById('email'),
-    password: document.getElementById('password'),
-    cPassword: document.getElementById('c-password'),
-  };
-
-  const showError = (input, message) => {
-    const inputGroup = input.parentElement.parentElement;
-    inputGroup.className = 'input-group error';
-    const errorMsg = inputGroup.querySelector('.error-msg');
-    errorMsg.textContent = message;
-  };
-
-  const checkRequired = (inputs, next) => {
-    for (const key in inputs) {
-      const input = inputs[key];
-      if (input.value.trim() === '') {
-        showError(input, 'This field is required');
-      }
-    }
-
-    // next();
-  };
-
-  const checkEmail = input => {
-    const regex =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-    if (regex.test(input.value.trim()) === false) {
-      showError(input, 'Email is not invalid');
-    }
-  };
-
-  // event
-  form.addEventListener('submit', e => {
-    e.preventDefault();
-
-    // checkRequired
-    checkRequired(inputs, checkEmail);
-    // checkUsername
-    // checkEmail
-    // checkPassword
-    // api call
-  });
+// get all Fields
+const form = document.querySelector('.form');
+const inputs = {
+  username: document.getElementById('username'),
+  email: document.getElementById('email'),
+  password: document.getElementById('password'),
+  cPassword: document.getElementById('c-password'),
+};
+const regex = {
+  email: {
+    pattern: /^[A-Za-z0-9+_.-]+@(.+)$/,
+    errorMsg: 'Email is not valid',
+  },
+  username: {
+    pattern: /^[a-zA-Z][a-zA-Z0-9]{3,13}[a-zA-Z]$/,
+    errorMsg:
+      'Username must consist of 5 to 15 characters, only letters and numbers are allowed, with no numbers at the beginning or the end',
+  },
+  password: {
+    pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+    errorMsg: 'Password minimum eight characters, at least one letter and one number',
+  },
 };
 
-validation();
+// logic
+function areAllFieldsHaveContent(inputs) {
+  let emptyFields = 0;
 
-// let data = {};
-// const inputs = document.querySelectorAll('.input-group .input');
+  for (const key in inputs) {
+    const input = inputs[key];
+    if (input.value.trim() === '') {
+      showError(input, 'This field is required');
+      emptyFields++;
+    }
+  }
 
-// inputs.forEach(input => {
-//   data = { ...data, [input.name]: input.value };
-// });
+  return emptyFields === 0;
+}
 
-// for (const key in data) {
-// }
+function isVaLidPattern(regex, input) {
+  if (regex.pattern.test(input.value.trim()) === true) {
+    return true;
+  }
+
+  showError(input, regex.errorMsg);
+  return false;
+}
+
+function showError(input, message) {
+  const inputGroup = input.parentElement.parentElement;
+  inputGroup.className = 'input-group error';
+  const errorMsg = inputGroup.querySelector('.error-msg');
+  errorMsg.textContent = message;
+}
+
+function clearErrors(inputs) {
+  for (const key in inputs) {
+    const input = inputs[key];
+    input.addEventListener('change', () => {
+      const inputGroup = input.parentElement.parentElement;
+      inputGroup.className = 'input-group';
+      const errorMsg = inputGroup.querySelector('.error-msg');
+      errorMsg.textContent = '';
+    });
+  }
+}
+
+function areTwoPasswordsTheSame(pass1, pass2) {
+  if (pass1.toLowerCase() === pass2.toLowerCase()) {
+    return true;
+  }
+
+  showError(inputs.cPassword, 'Password and confirmation password should be the same');
+  return false;
+}
+
+// event
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  // clearErrors
+  clearErrors(inputs);
+
+  // checkRequired
+  if (areAllFieldsHaveContent(inputs) === true) {
+    let check1 = isVaLidPattern(regex.username, inputs.username);
+    let check2 = isVaLidPattern(regex.email, inputs.email);
+    let check3 = isVaLidPattern(regex.password, inputs.password);
+    let check4 = areTwoPasswordsTheSame(inputs.password, inputs.cPassword);
+
+    if (check1 && check2 && check3 && check4) {
+      // api call
+      console.log('api call');
+    }
+  }
+});
