63429e8e71d04527c2dc8872d25a65520e7df4d3
ahmed-m-abdelfatah <ahmedmo.engineer@gmail.com>
2022-12-09 13:14:28 +0200

    checkpoint

--- src/assets/js/validation.js
+++ src/assets/js/validation.js
@@ -1,28 +1,50 @@
 const validation = () => {
   // get all Fields
   const form = document.querySelector('.form');
-  const usernameInput = document.querySelectorAll('.input-group .input[name="username"]');
-  const emailInput = document.querySelectorAll('.input-group .input[name="email"]');
-  const passwordInput = document.querySelectorAll('.input-group .input[name="password"]');
-  const cPasswordInput = document.querySelectorAll('.input-group .input[name="password_confirmation"]');
+  const inputs = {
+    username: document.getElementById('username'),
+    email: document.getElementById('email'),
+    password: document.getElementById('password'),
+    cPassword: document.getElementById('c-password'),
+  };
 
-  function checkEmail(input) {
+  const showError = (input, message) => {
+    const inputGroup = input.parentElement.parentElement;
+    inputGroup.className = 'input-group error';
+    const errorMsg = inputGroup.querySelector('.error-msg');
+    errorMsg.textContent = message;
+  };
+
+  const checkRequired = (inputs, next) => {
+    for (const key in inputs) {
+      const input = inputs[key];
+      if (input.value.trim() === '') {
+        showError(input, 'This field is required');
+      }
+    }
+
+    // next();
+  };
+
+  const checkEmail = input => {
     const regex =
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if (regex.test(input.value) === false) {
+    if (regex.test(input.value.trim()) === false) {
       showError(input, 'Email is not invalid');
     }
-  }
+  };
 
   // event
   form.addEventListener('submit', e => {
     e.preventDefault();
 
     // checkRequired
+    checkRequired(inputs, checkEmail);
     // checkUsername
     // checkEmail
     // checkPassword
+    // api call
   });
 };
 
